2c5cbbde011c5137558a19cf0ebfcb40129284b3
Torrin Lisi <torrinlisi@gmail.com>
2021-04-15 22:13:56 -0400

    start microservice stuff

--- services/tasks/server.js
+++ services/tasks/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const port = process.env.PORT || '3000';
+const port = process.env.APPID || '3000';
 app.set('port', port);
 
 console.log(`Task service listening on port ${port}`);
